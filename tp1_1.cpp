b2c540475a6c12eec8d189e691794208339198ff
ScidaP <patricioscida@gmail.com>
2021-03-30 15:05:26 -0300

    Primer Ejercicio

--- tp1_1.cpp
+++ tp1_1.cpp
@@ -1,5 +1,19 @@
 #include <stdio.h>
 
 int main() {
-    
+    int numero= 1;
+    int *pNumero= &numero;
+
+    printf("Contenido del puntero: %d\n", *pNumero);
+
+    printf("Direccion de memoria almacenada por el puntero: %p\n", pNumero);
+
+    printf("Direccion de memoria de la variable: %p\n", &numero);
+
+    printf("Direccion de memoria del puntero: %p\n", &pNumero);
+
+    printf("Tamaño de la variable: %d", sizeof(numero));
+
+    getchar();
+    return 0;
 }
